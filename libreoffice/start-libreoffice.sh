ecc37c2314d220abdfbeef03858de45d63d86b90
Zoltan Farkas <f.zoli@mailbox.hu>
2018-12-20 12:12:30 +0100

    handle sigint

--- libreoffice/start-libreoffice.sh
+++ libreoffice/start-libreoffice.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-soffice --nologo --headless --nofirststartwizard --accept='socket,host=0.0.0.0,port=8100,tcpNoDelay=1;urp'
+exec soffice --nologo --headless --nofirststartwizard --accept='socket,host=0.0.0.0,port=8100,tcpNoDelay=1;urp'
